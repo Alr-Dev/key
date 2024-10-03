013a4d51c525efc34a928ad8843bc8197967ddee
alr_dev <157879926+Alr-Dev@users.noreply.github.com>
2024-10-03 20:02:55 -0300

    Update keys

--- keys.lua
+++ keys.lua
@@ -8,7 +8,7 @@ local StarterGui = game:GetService("StarterGui") -- Adicionado StarterGui
 local OrionLib1 = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))() -- Renamed to OrionLib1
 
 -- Local API Keys (Replace with your actual keys)
-local validKeys = {SPJAIM-0xu%"=),UX_*-=|DZhvvcY\}
+local validKeys = {"SPJAIM-0x<w]9:)=siV-&V;yky/\vT"}
 
 -- Function to verify API Key
 local function verifyApiKey(apiKey, callback)
